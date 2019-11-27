7d5c09160bcf80563b96f22185f0d3ccc7fdd05a
Charles Tam <rngesus.wept+github@gmail.com>
2019-11-26 21:51:28 -0500

    asdf

--- trivia_plus/trivia.py
+++ trivia_plus/trivia.py
@@ -252,7 +252,7 @@ class Trivia(commands.Cog):
     async def trivia_list(self, ctx: commands.Context):
         """List available trivia categories."""
         lists = set(p.stem for p in self._all_lists())
-        await self.ctx.send(repr(cog_data_path(self)))
+        await ctx.send(repr(cog_data_path(self)))
         if await ctx.embed_requested():
             await ctx.send(
                 embed=discord.Embed(
