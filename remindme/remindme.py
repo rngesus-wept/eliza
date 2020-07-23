9bd2beeea876fcc2247fd2fc20b9ed6f25af1f02
Charles Tam <rngesus.wept+github@gmail.com>
2020-07-23 10:50:37 -0400

    Add logging statement to remindme

--- remindme/remindme.py
+++ remindme/remindme.py
@@ -13,7 +13,7 @@ from redbot.core import Config
 from redbot.core.bot import Red
 
 
-log = logging.getLogger("remindme")
+log = logging.getLogger("red.eliza.remindme")
 log.setLevel(logging.INFO)
 
 
@@ -65,6 +65,9 @@ class RemindMe(commands.Cog):
       data = await self.config.all_users()
       for user_id in data:
         user = self.bot.get_user(user_id)
+        if not user:
+          log.warn(f'Unable to find user with user_id {user_id}')
+          continue
         async with self.config.user(user).reminders() as reminders:
           to_remove = []
           for timestamp, message in reminders.items():
