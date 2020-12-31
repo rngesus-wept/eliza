c1ddac6f0bd5eaaf1247eb184dfcb8e969d56e8c
Charles Tam <rngesus.wept+github@gmail.com>
2020-12-31 16:51:27 -0500

    Checkpoint

--- team_tracker/team_tracker.py
+++ team_tracker/team_tracker.py
@@ -8,12 +8,14 @@ from redbot.core import Config
 from redbot.core import checks
 from redbot.core.bot import Red
 
+import .nl
+
 
 CONFIG_ID = 0x51C6FD2FD8E37013
 
 DEFAULT_GLOBAL_SETTINGS = {
     'teams': {},
-    'token': None,
+    'secret': None,
 }
 
 DEFAULT_GUILD_SETTINGS = {
@@ -24,7 +26,20 @@ DEFAULT_GUILD_SETTINGS = {
 DEFAULT_USER_SETTINGS = {
     'display_name': '',  # Is this necessary?
     'team_id': -1,
+    'secret': None,  # salt for transmitting user ID
 }
+
+
+def registration_url(user_id, token=None):
+  return f'http://localhost:8000/register/{user_id}'
+
+
+def get_team_url(user_id, token=None):
+  pass
+
+
+def display_user_full(user):
+  return f'{user.display_name} ({user.name}#{user.discriminator})'
 
 
 class TeamTracker(commands.Cog):
@@ -37,15 +52,21 @@ class TeamTracker(commands.Cog):
     self.config.register_guild(**DEFAULT_GUILD_SETTINGS)
     self.config.register_user(**DEFAULT_USER_SETTINGS)
 
-    self.token = await self.config.token()
-
   async def initialize(self):
+    # Load config information to internal memory
+    self.token = await self.config.secret()
+
+    self.guilds = {}  # guild_id integer -> Guild object
+    self.admin_channels = {}  # guild_id integer -> TextChannel object
+    for guild_id in await self.config.all_guilds():
+      await self._load_guild(guild_id=guild_id)
+
     self.bot.add_listener(self.member_join, 'on_member_join')
 
   async def member_join(self, member):
     # Reminder: A member is a user x guild combination; that is, the same user
     # in two different guilds will be represented by two different members!
-
+    user_id = member.id
 
   ######### General stuff
 
@@ -63,10 +84,31 @@ class TeamTracker(commands.Cog):
     """Team affiliation administrator functions."""
     pass
 
+  @_admin.command(name='enable')
+  @commands.guild_only()
+  @checks.mod_or_permissions(administrator=True)
+  async def admin_enable(self, ctx: commands.Context):
+    if ctx.guild.id in self.guilds:
+      await ctx.send('Team management is already enabled in this guild.')
+      return
+    await self._enable_guild(guild=ctx.guild)
+    await ctx.send('Team management enabled.')
+
+  @_admin.command(name='disable')
+  @commands.guild_only()
+  @checks.mod_or_permissions(administrator=True)
+  async def admin_disable(self, ctx: commands.Context):
+    if ctx.guild.id not in self.guilds:
+      await ctx.send('Team management is already disabled in this guild.')
+      return
+    await self._disable_guild(guild=ctx.guild)
+    await ctx.send('Team management disabled.')
+
   @_admin.command(name='channel')
   @commands.guild_only()
   @checks.mod_or_permissions(administrator=True)
-  async def admin_channel(self, ctx: commands.Context):
+  async def admin_channel(
+      self, ctx: commands.Context, channel: discord.TextChannel=None):
     """Sets or displays the channel for admin message output.
 
     Set on a per-guild basis; multiple guilds may receive admin messages if
@@ -76,5 +118,137 @@ class TeamTracker(commands.Cog):
     a channel as an argument to point to that channel for the current guild.
     Provide any other string as an argument to unset the value for the current
     guild (though for sanity's sake it should probably be something
-    False-looking.
+    False-looking).
     """
+    the_channel = self.admin_channels.get(ctx.guild.id, None)
+    if channel is None:
+      if the_channel is None:
+        await ctx.send(
+            'No admin channel is set for team management in this guild.'
+            ' To set one, provide this command with a reference to the'
+            ' desired channel.')
+      else:
+        message = ['Admin messages for team management are routed to',
+                   f' this guild\'s {the_channel.mention}']
+        others = len(ch for ch in self.admin_channels.values() if ch) - 1
+        if others:
+          message.append(f' (and {others} other channel{nl.s(others)})')
+        message.append('.')
+
+        await ctx.send(''.join(message))
+    else:
+      await self._set_admin_channel(guild=ctx.guild, channel=channel)
+
+      message = ['Team management admin messages are now routed to',
+                 f' {channel.mention}.']
+      await ctx.send(''.join(message))
+
+  @_admin.command(name='secret')
+  @checks.mod_or_permissions(administrator=True)
+  async def admin_secret(self, ctx: commands.Context, token: str=None):
+    """Sets or displays the token used for talking to silenda."""
+    the_token = self.token
+    if token is None:
+      await ctx.author.send(f'Team management secret: {the_token}')
+    else:
+      await self._set_secret(token)
+      message = [display_user_full(ctx.author),
+                 'set the team management secret to',
+                 f'`{self.token}`.']
+      if the_token:
+        message.append(f'(was `{the_token}`)')
+      await self.admin_msg(' '.join(message))
+
+  async def admin_msg(self, message):
+    """Sends a message to all admin channels."""
+    for channel in self.admin_channels.values():
+      if channel:
+        await channel.send(message)
+
+  ######## Data management helper functions
+  ## These are here to make sure that writes to the cog's internal memory are
+  ## reflected to the datastore, and the reads from the cog's datastore are
+  ## reflected to its internal memory.
+
+  async def _load_guild(self,
+                        guild: discord.Guild = None,
+                        guild_id: int = None):
+    if guild is None:
+      guild = self.bot.get_guild(guild_id)
+    if not await self.config.guild(guild).enabled():
+      return
+
+    self.guilds[guild.id] = guild
+    await self._load_admin_channel(guild=guild)
+
+  async def _enable_guild(self,
+                          guild: discord.Guild = None,
+                          guild_id: int = None):
+    if guild is None:
+      guild = self.bot.get_guild(guild_id)
+    if guild.id in self.guilds:
+      return
+
+    await self.config.guild(guild).enabled.set(True)
+    self.guilds[guild.id] = guild
+
+    await self._load_admin_channel(guild=guild)
+
+  async def _disable_guild(self,
+                           guild: discord.Guild = None,
+                           guild_id: int = None):
+    if guild is None:
+      guild = self.bot.get_guild(guild_id)
+    if guild.id not in self.guilds:
+      return
+
+    await self.config.guild(guild).enabled.set(False)
+    del self.guilds[guild.id]
+    await self._unload_admin_channel(guild=guild)
+
+  async def _load_admin_channel(self,
+                                guild: discord.Guild = None,
+                                guild_id: int = None):
+    if guild is None:
+      guild = self.bot.get_guild(guild_id)
+
+    admin_channel_id = await self.config.guild(guild).admin_channel()
+    if not admin_channel_id:
+      self.admin_channels[guild.id] = None
+    else:
+      self.admin_channels[guild.id] = self.bot.get_channel(admin_channel_id)
+
+  async def _unload_admin_channel(self,
+                                  guild: discord.Guild = None,
+                                  guild_id: int = None):
+    if guild is None:
+      guild = self.bot.get_guild(guild_id)
+    if guild.id in self.admin_channels:
+      del self.admin_channels[guild.id]
+
+  async def _set_admin_channel(self,
+                               guild: discord.Guild = None,
+                               guild_id: int = None,
+                               channel: discord.TextChannel = None,
+                               channel_id: int = None):
+    if guild is None:
+      guild = self.bot.get_guild(guild_id)
+    await self._enable_guild(guild=guild)
+
+    if channel is None:
+      channel = self.bot.get_channel(channel_id)
+
+    await self.config.guild(guild).admin_channel.set(channel.id)
+    self.admin_channels[guild.id] = channel
+
+  async def _unset_admin_channel(self,
+                                 guild: discord.Guild = None,
+                                 guild_id: int = None):
+    if guild is None:
+      guild = self.bot.get_guild(guild_id)
+    await self.config.guild(guild).admin_channel.set(0)
+    self.admin_channels(guild.id) = None
+
+  async def _set_secret(self, token):
+    await self.config.secret.set(token)
+    self.token = token
