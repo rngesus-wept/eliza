ba25ac385c5b79ad3208688599529e132dd6fec5
Charles Tam <rngesus.wept+github@gmail.com>
2021-01-01 14:04:57 -0500

    checkpoint

--- team_tracker/team_tracker.py
+++ team_tracker/team_tracker.py
@@ -1,8 +1,11 @@
 """Cog for tracking team affiliations globally."""
 
 import asyncio
+import logging
+import time
 
 import discord
+from discord.ext import tasks
 from redbot.core import commands
 from redbot.core import Config
 from redbot.core import checks
@@ -12,11 +15,14 @@ from redbot.core.utils import mod
 from . import nl
 
 
+log = logging.getLogger('red.eliza.team_tracker')
+
 CONFIG_ID = 0x51C6FD2FD8E37013
 
 DEFAULT_GLOBAL_SETTINGS = {
     'teams': {},
     'secret': None,
+    ## TODO: Make registration URL a setting?
 }
 
 DEFAULT_GUILD_SETTINGS = {
@@ -28,8 +34,12 @@ DEFAULT_USER_SETTINGS = {
     'display_name': '',  # Is this necessary?
     'team_id': -1,
     'secret': None,  # salt for transmitting user ID
-    'last_update': 0,  # time.time()
+    'last_updated': 0,  # time.time()
+    'do_not_message': False,
 }
+
+PERMIT_OVERWRITE = discord.PermissionOverwrite(view_channel=True)
+FORBID_OVERWRITE = discord.PermissionOverwrite(view_channel=False)
 
 
 def registration_url(user_id, token=None):
@@ -43,6 +53,51 @@ def get_team_url(user_id, token=None):
 
 def display_user_full(user):
   return f'{user.display_name} ({user.name}#{user.discriminator})'
+
+
+class TeamData(object):
+  """Structured data collected about teams."""
+  team_id = -1
+  display_name = None
+  username = None
+  channels = []  # list of objects, but serializes as a list of ids
+  users = []  # list of objects, but serializes as a list of ids
+  last_updated = 0  # time.time()
+
+  @staticmethod
+  async def read(bot: Red, data: dict):
+    obj = TeamData()
+    obj.team_id = data['team_id']
+    obj.display_name = data['display_name']
+    obj.username = data['username']
+    obj.channels = [await bot.get_channel(channel_id)
+                     for channel_id in data['channels']]
+    obj.users = [await bot.get_user(user_id)
+                  for user_id in data['users']]
+    obj.last_updated = data['last_updated']
+    return obj
+
+  async def reload(self):
+    # Re-retrieves all Channel and User objects
+    self.channels = [await bot.get_channel(channel.id)
+                     for channel in self.channels]
+    self.users = [await bot.get_user(user.id)
+                  for user in self.users]
+
+  async def write(self, config: Config):
+    serialized = {
+        'team_id': self.team_id,
+        'display_name': self.display_name,
+        'username': self.username,
+        'channels': [channel.id for channel in self.channels],
+        'users': [user.id for user in self.users],
+        'last_updated': self.last_updated,
+    }
+    await config.set_raw('teams', self.team_id, value=serialized)
+    await config.set_raw('teams', self.username, value=serialized)
+    # async with config.teams() as teams:
+    #   teams[self.team_id] = serialized
+    #   teams[self.username] = serialized
 
 
 class TeamTracker(commands.Cog):
@@ -77,7 +132,35 @@ class TeamTracker(commands.Cog):
   async def member_join(self, member):
     # Reminder: A member is a user x guild combination; that is, the same user
     # in two different guilds will be represented by two different members!
-    user_id = member.id
+    if member.bot:
+      return
+    team_id = await self.config.user(member).team_id()
+    if team_id == -1:
+      await self.registration_prompt(member)
+    else:
+      team_data = self.teams[team_id]
+      for channel in team_data.channels:
+        if channel.guild == member.guild:
+          await self._permit_member_in_channel(
+              member, channel,
+              f'Adding registered user {display_user_full(member)}')
+
+  async def registration_prompt(self, user, bypass_ignore=False):
+    do_not_message = await self.config.user(user).do_not_message()
+    if do_not_message and not bypass_ignore:
+      return
+
+    my_prefix = (await self.bot._prefix_cache.get_prefixes())[0]
+    await user.send(
+        'Hello! You\'re receiving this message because you joined the server'
+        f' **{user.guild.name}** and are not associated with any particular'
+        ' team.\n'
+        f'    * To associate yourself with a team, visit <{registration_url(user.id)}>.\n'
+        '        Please do not share this link other players, nor click on'
+        ' links of this form sent to you by other players.\n'
+        '    * To never receive team-related messages from me again, respond'
+        f' with `{my_prefix}team ignore`. (Opt back in with `{my_prefix}team'
+        ' unignore`.)')
 
   ######### General stuff
 
@@ -217,6 +300,12 @@ class TeamTracker(commands.Cog):
                  f' {channel.mention}.']
       await ctx.send(''.join(message))
 
+  @_admin.command(name='ping')
+  @commands.guild_only()
+  @checks.mod_or_permissions(administrator=True)
+  async def admin_local_ping(self, ctx: commands.Context, team_identifier):
+    pass
+
   @_admin.command(name='secret')
   @checks.mod_or_permissions(administrator=True)
   async def admin_secret(self, ctx: commands.Context, token: str=None):
@@ -240,6 +329,7 @@ class TeamTracker(commands.Cog):
         await channel.send(message)
 
   ######## Data management helper functions
+
   ## These are here to make sure that writes to the cog's internal memory are
   ## reflected to the datastore, and the reads from the cog's datastore are
   ## reflected to its internal memory.
@@ -327,38 +417,114 @@ class TeamTracker(commands.Cog):
     await self.config.secret.set(token)
     self.token = token
 
+  async def _get_team_data(self, identifier):
+    data = await self.config.get_raw('teams', identifier)
+    return await TeamData.read(self.bot, data)
 
-class TeamData(object):
-  """Structured data collected about teams."""
-  team_id = -1
-  display_name = None
-  username = None
-  channels = []  # list of objects, but serializes as a list of ids
-  users = []  # list of objects, but serializes as a list of ids
-  last_updated = 0  # time.time()
+  async def _add_user_to_team(self, user: discord.User, team: TeamData):
+    if user in team.users:
+      log.debug(f'User {display_user_full(user)} is already on {team.username}.')
+      return
+    team.users.append(user)
+    await team.write(self.config)
 
-  @staticmethod
-  async def read(bot: Red, data: dict):
-    obj = TeamData()
-    obj.team_id = data['team_id']
-    obj.display_name = data['display_name']
-    obj.username = data['username']
-    obj.channels = [await bot.get_channel(channel_id)
-                     for channel_id in data['channels']]
-    obj.users = [await bot.get_user(user_id)
-                  for user_id in data['users']]
-    obj.last_updated = data['last_updated']
-    return obj
+    await self.config.user(user).team_id.set(team.team_id)
+    await self.config.user(user).last_updated.set(time.time())
 
-  async def write(self, config: Config):
-    serialized = {
-        'team_id': self.team_id,
-        'display_name': self.display_name,
-        'username': self.username,
-        'channels': [channel.id for channel in self.channels],
-        'users': [user.id for user in self.users],
-        'last_updated': time.time(),
-    }
-    async with config.teams() as teams:
-      teams[self.team_id] = serialized
-      teams[self.username] = serialized
+    for channel in team.channels:
+      await self._permit_user_in_channel(
+          user, channel, f'Adding {user.name} to {team.username}')
+
+  async def _remove_user_from_team(self, user: discord.User, team: TeamData):
+    if user not in team.users:
+      log.debug(f'User {display_user_full(user)} is not on {team.username}.')
+      return
+    team.users.remove(user)
+    await team.write(self.config)
+
+    await self.config.user(user).team_id.set(-1)
+    await self.config.user(user).last_updated.set(time.time())
+
+    for channel in team.channels:
+      await self._forbid_user_in_channel(
+          user, channel, f'Removing {user.name} from {team.username}')
+
+  ## NOTE: Channels will be created such that users have all the necessary
+  ## permissions EXCEPT being able to see the channel. This means that
+  ## general user permissions on all channels is to be gated solely on
+  ## visibility and no other permission.
+
+  async def _permit_user_in_channel(self, user: discord.User,
+                                    channel: discord.abc.GuildChannel,
+                                    reason: str = None):
+    guild = channel.guild
+    member = await guild.get_member(user.id)
+    if member is None:
+      # User is not in the guild containing the channel
+      return
+    await self._permit_member_in_channel(member, channel, reason=reason)
+
+
+  async def _permit_member_in_channel(self, member: discord.Member,
+                                      channel: discord.abc.GuildChannel,
+                                      reason: str = None):
+    await channel.set_permissions(member, overwrite=PERMIT_OVERWRITE,
+                                  reason=reason)
+
+
+  async def _forbid_user_in_channel(self, user: discord.User,
+                                    channel: discord.abc.GuildChannel,
+                                    reason: str = None):
+    guild = channel.guild
+    member = await guild.get_member(user.id)
+    if member is None:
+      # User is not in the guild containing the channel
+      return
+    await self._forbit_member_in_channel(member, channel, reason=reason)
+
+  async def _forbid_member_in_channel(self, member: discord.Member,
+                                      channel: discord.abc.GuildChannel,
+                                      reason: str = None):
+    await channel.set_permissions(member, overwrite=FORBID_OVERWRITE,
+                                  reason=reason)
+
+  async def _permit_team_in_channel(self, team: TeamData,
+                                    channel: discord.abc.GuildChannel):
+    team.channels.append(channel)
+    await team.write(self.config)
+
+    for user in team.users:
+      await self._permit_user_in_channel(
+          user, channel, f'Adding {team.username}')
+
+  async def _forbid_team_in_channel(self, team: TeamData,
+                                    channel: discord.abc.GuildChannel):
+    team.channels.remove(channel)
+    await team.write(self.config)
+
+    for user in team.users:
+      await self._forbid_user_in_channel(
+          user, channel, f'Removing {team.username}')
+
+  ## DEBUG
+
+  @commands.command(name='testjoin')
+  async def simulate_join(self, ctx: commands.Context,
+                          member: discord.Member = None):
+    if member is not None:
+      await self.member_join(member)
+    else:
+      await self.member_join(ctx.author)
+
+  @commands.command(name='tt')
+  async def my_debug(self, ctx: commands.Context):
+    # team_data = TeamData()
+    # team_data.team_id = 1
+    # team_data.display_name = 'Test team'
+    # team_data.username = 'test'
+    # await team_data.write(self.config)
+    # await self._add_user_to_team(ctx.author, team_data)
+
+    await self.config.user(ctx.author).team_id.set(2)
+    data = await self.config.user(ctx.author)()
+    await ctx.send(repr(data))
