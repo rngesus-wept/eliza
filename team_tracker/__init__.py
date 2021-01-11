e17ca903c6639de8bfd2f798565dbcbb610219f2
rngesus-wept <33389307+rngesus-wept@users.noreply.github.com>
2021-01-10 19:43:25 -0500

    Team tracker (#21)
    * Initial commit
    
    * Checkpoint
    
    * Checkpoint
    
    * checkpoint
    
    * unhelpful commit message
    
    * Implement forget
    
    * Implement [p]show, mostly
    
    * Basic channel creation and team viewing
    
    * typo
    
    * Checkpoint
    
    * Start planning for participant role
    
    * Check for null channels
    
    * stuff
    
    * asdf

---
+++ team_tracker/__init__.py
@@ -0,0 +1,43 @@
+"""Tracks a persistent identity for users it sees in particular servers.
+
+Whenever a user the cog doesn't recognize joins a server where the cog is
+active, prompts the user, via DM, to indicate a team affiliation. In that
+server and in every server where the cog is active, assigns that user to that
+team.
+
+This cog uses the [p]team command group.
+
+Admin/Mod:
+  [p]enable          Enables the cog in the current server.
+  [p]disable         Disables the cog in the current server.
+  [p]channel [teams]  Creates a private channel visible to admins and the
+                      designated teams.
+  [p]channel add
+  [p]channel remove
+!  [p]update
+  [p]stderr [chnl]   Sets admin messages to go to the designated channel.
+                     All admin messages go to all admin channels.
+!  [p]search [foo]    Searches for all teams whose names contain regex `foo`.
+!  [p]ping [team] [msg]   DMs all members of `team` with `msg`.
+  [p]show [id]       Shows information about the team
+  [p]reset           Resets all memory for all users, globally.
+
+User:
+  [p]whois <user>    States the team affiliation of the input user.
+  [p]whoami          States the team affiliation of the sending user.
+  [p]register [user]   Manually initiates the registration process for the
+                       sending user.
+  [p]forget [user]   Removes the sending user from their team affiliation.
+  [p]ignore [user]   Opts out of receiving team_tracker pings.
+  [p]unignore [user] Opts back in to receiving team_tracker pings.
+"""
+# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+from redbot.core.bot import Red
+from .team_tracker import TeamTracker
+
+
+async def setup(bot: Red):
+  tt_module = TeamTracker(bot)
+  await tt_module.initialize()
+  bot.add_cog(tt_module)
---
+++ team_tracker/nl.py
@@ -0,0 +1,8 @@
+def s(count):
+  return '' if count == 1 else 's'
+
+def es(count):
+  return '' if count == 1 else 'es'
+
+def ies(count):
+  return 'y' if count == 1 else 'ies'
